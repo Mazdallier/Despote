d001dc908a0082105a6536b84ee57840cc17166f
Yves <Mazdallier@users.noreply.github.com>
2015-01-14 11:27:17 +0100

    Update Despote.java

--- src/main/java/despote/Despote.java
+++ src/main/java/despote/Despote.java
@@ -6,9 +6,32 @@
  * http://www.gnu.org/licenses/lgpl-3.0.txt
  ******************************************************************************/
 
+import despote.lib.RefStrings
 
-@Mod(modid = , name = , version = )
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-Public class Despote {
+
+@Mod(modid = RefStrings.MODID , name = RefStrings.NAME , version = RefStrings.VERSION)
+
+public class Despote {
+  
+  @EventHandler
+  public static void PreLoad(FMLPreInitializationEvent PreEvent){
+    
+  }
+  
+  @EventHandler
+  public static void load(FMLInitializationEvent event){
+    
+  }
+  
+  @EventHandler
+  public static void PostLoad(FMLPostInitializationEvent PostEvent){
+    
+  }
   
 }
