fca40cc7e68b3525e9cdfbca729b8dd1e8e49da4
Yves <Mazdallier@users.noreply.github.com>
2015-01-14 11:13:15 +0100

    Update and rename RefString.java to RefStrings.java

--- src/main/java/despote/lib/RefStrings.java
+++ src/main/java/despote/lib/RefStrings.java
@@ -1,6 +1,6 @@
 package despote.lib;
 
-public class RefString{
+public class RefStrings{
   public static final String MODID = "despote";
   public static final String NAME = "Despotic Mod";
   public static final String VERSION = "0.1";
