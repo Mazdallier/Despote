6d3c9ce64f197889334e0c0299a394d6229753f2
Yves <Mazdallier@users.noreply.github.com>
2015-01-14 11:08:52 +0100

    Create RefString.java

---
+++ src/main/java/despote/lib/RefString.java
@@ -0,0 +1,7 @@
+package despote.lib;
+
+public class RefString{
+  public static final String MODID = "despote";
+  public static final String NAME = "Despotic Mod";
+  public static final String VERSION = "0.1";
+}
