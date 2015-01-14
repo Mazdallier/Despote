406fd54482bbe91c7354eed216ef24ef24935a2b
Yves <Mazdallier@users.noreply.github.com>
2015-01-14 11:36:10 +0100

    Update RefStrings.java

--- src/main/java/despote/lib/RefStrings.java
+++ src/main/java/despote/lib/RefStrings.java
@@ -4,4 +4,6 @@ public class RefStrings{
   public static final String MODID = "despote";
   public static final String NAME = "Despotic Mod";
   public static final String VERSION = "0.1";
+  public static final String CLIENTSIDE = "despote.proxy.ClientProxy";
+  public static final String SERVERSIDE = "despote.proxy.ServerProxy";
 }
