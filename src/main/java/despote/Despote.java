4f63fe127f4db52ac2d6a0ebef2c4e807d0f7d44
Yves <Mazdallier@users.noreply.github.com>
2015-01-14 10:11:11 +0100

    Create Despote.java

---
+++ src/main/java/despote/Despote.java
@@ -0,0 +1,7 @@
+/*******************************************************************************
+ * Copyright (c) 2011-2014 Mazdallier.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v3
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ ******************************************************************************/
